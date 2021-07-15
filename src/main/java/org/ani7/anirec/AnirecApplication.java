2c3a9715dd7ceb658eb22d304944995fc3f87fa6
Phillip Vo <phillip.t.vo@gmail.com>
2021-07-14 19:39:49 -0700

    First commit

---
+++ src/main/java/org/ani7/anirec/AnirecApplication.java
@@ -0,0 +1,13 @@
+package org.ani7.anirec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnirecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnirecApplication.class, args);
+	}
+
+}
